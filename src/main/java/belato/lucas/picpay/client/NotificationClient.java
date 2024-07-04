c9685c016cf19ae043cf697debf690ad20f32e4f
LSB30 <lukinhaasantana777@gmail.com>
2024-07-04 15:20:15 -0300

    Feat: ⚡ Create NotificationClient

---
+++ src/main/java/belato/lucas/picpay/client/NotificationClient.java
@@ -0,0 +1,15 @@
+package belato.lucas.picpay.client;
+
+import belato.lucas.picpay.entities.Transfer;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(
+        url = "${client.notification-service.url}"
+)
+public interface NotificationClient {
+    @PostMapping
+    ResponseEntity<Void> sendNotification(@RequestBody Transfer transfer);
+}
