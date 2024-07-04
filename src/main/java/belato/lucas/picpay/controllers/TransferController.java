f54c6e95009a73c39fde7c93e9a619c9d8d8c030
LSB30 <lukinhaasantana777@gmail.com>
2024-07-04 15:26:22 -0300

    Feat: ⚡ Create TransferController

---
+++ src/main/java/belato/lucas/picpay/controllers/TransferController.java
@@ -0,0 +1,9 @@
+package belato.lucas.picpay.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransferController {
+
+    private final TransferSerivice transferSerivice;
+}
