b292ce141a88d78da8fb5d3edf5a913dd0e42ecd
LSB30 <lukinhaasantana777@gmail.com>
2024-07-04 11:13:59 -0300

    Feat: ⚡ Create new exception

--- src/main/java/belato/lucas/picpay/controllers/RestExceptionHandler.java
+++ src/main/java/belato/lucas/picpay/controllers/RestExceptionHandler.java
@@ -1,7 +1,9 @@
 package belato.lucas.picpay.controllers;
 
 import belato.lucas.picpay.expections.PicpayExpection;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,4 +15,20 @@ public class RestExceptionHandler {
         return e.toProblemDetail();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var fieldErros = e.getFieldErrors()
+                .stream()
+                .map(f -> new invalidParam(f.getField(),f.getDefaultMessage()))
+                .toList();
+
+        var pb = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        pb.setTitle("Your request parameters didn't validate.");
+        pb.setProperty("invalid-params", fieldErros);
+        return null;
+
+
+    }
+
+    private record invalidParam(String name, String reason){}
 }
